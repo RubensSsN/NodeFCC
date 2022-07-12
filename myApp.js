6da296e0b3005c13e35ed13544693e7710f3d1b8
RubensSamuel <contatorubeno@gmail.com>
2022-07-12 19:38:27 -0300

    EnvTeste

--- myApp.js
+++ myApp.js
@@ -1,45 +1,33 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-
-const MESSAGE_STYLE = process.env.MESSAGE_STYLE
-
-
+//tried this in place of mySecret too. const secret = process.env.MESSAGE_STYLE
 
 /* app.get('/', function(req, res) {
   res.send('Hello Express');
 }); */
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
+const mySecret = process.env.MESSAGE_STYLE;
 
-app.get('/json', function(req, res) {
-  const helloJSON = 'Hello json';
-  const message = MESSAGE_STYLE === 'uppercase' ?
-  helloJSON.toUpperCase() : helloJSON;
+app.get("/json", function (req, res) {
+  const helloJSON = "Hello json";
+  let message;
+  if (process.env.MESSAGE_STYLE === "uppercase") { // I also put process.env.MESSAGE_STYLE in place of mySecret
+    message = helloJSON.toUpperCase();
+  } else {
+    message = helloJSON;
+  }
   const data = {
-    "message": message
+    message: message,
   };
   res.json(data);
 });
 
-app.get('/', function(req, res) {
-  const filePath = __dirname + '/views/index.html';
+app.get("/", function (req, res) {
+  const filePath = __dirname + "/views/index.html";
   console.log(filePath);
   res.sendFile(filePath);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
