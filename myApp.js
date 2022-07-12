778edb264571ce7507d85f7e4172667cc265f189
RubensSamuel <contatorubeno@gmail.com>
2022-07-12 00:00:18 -0300

    Teste06

--- myApp.js
+++ myApp.js
@@ -1,26 +1,43 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-//console.log("Hello World");
+console.log('Hello World');
+
+
 
 /* app.get('/', function(req, res) {
   res.send('Hello Express');
 }); */
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get("/", function (req, res) {
-  const filePath = __dirname + "/views/index.html";
+app.get('/', function(req, res) {
+  const filePath = __dirname + '/views/index.html';
   console.log(filePath);
   res.sendFile(filePath);
 });
 
-app.get("/json", function (req, res) {
+app.get('/json', function(req, res) {
   const data = {
-    message: "Hello json",
+    "message": "Hello json"
   };
   res.json(data);
-});
+})
 
 
-module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
